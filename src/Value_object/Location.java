255b14ee650bdf903516b8ca246fcd224d54f54d
peackyyyyyy <hugo.malamaire06800@gmail.com>
2021-05-11 16:58:00 +0200

    add Location value object

---
+++ src/Value_object/Location.java
@@ -0,0 +1,12 @@
+package Value_object;
+
+public class Location {
+
+    Voiture voiture;
+    Client client;
+
+    Location(Voiture voiture, Client client){
+        this.voiture = voiture;
+        this.client = client;
+    }
+}
