b81da57caed2957319f22f64d59d72d302803136
Théo <theomarion08@gmail.com>
2021-05-18 09:43:51 +0200

    merge

--- src/value_object/Categorie/Confort.java
+++ src/value_object/Categorie/Confort.java
@@ -38,9 +38,10 @@ public class Confort implements ICategorie {
     public int getTarif() {
         return price;
     }
-}
-
     @Override
     public String toString() {
     	return "Confort ";
     }
+}
+
+    
--- src/value_object/Categorie/Economique.java
+++ src/value_object/Categorie/Economique.java
@@ -38,9 +38,9 @@ public class Economique implements ICategorie {
     public int getTarif() {
         return price;
     }
-}
-
     @Override
     public String toString() {
     	return "Economique";
     }
+}
+
