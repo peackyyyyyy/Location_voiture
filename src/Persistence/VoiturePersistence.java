8cfd6a42d168e1e94432db625e42be1df3675e3b
peackyyyyyy <hugo.malamaire06800@gmail.com>
2021-06-13 11:01:44 +0200

    merge

--- src/Persistence/VoiturePersistence.java
+++ src/Persistence/VoiturePersistence.java
@@ -1,96 +1,94 @@
 package Persistence;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import value_object.Voiture;
 
-import java.sql.*;
-import java.util.ArrayList;
-
-public class VoiturePersistence extends JdbcConnexion{
-
-    Statement conn;
-    Connection connexion;
-    CategoriePersistence cp;
-    CarburantPersistence carbup;
-    StatePersistence stp;
+public class VoiturePersistence extends JdbcConnexion {
+    private Statement conn;
+    private Connection connexion;
+    private CategoriePersistence cp;
+    private CarburantPersistence carbup;
+    private StatePersistence stp;
+    private ArrayList<Voiture> listeVoiture;
 
     public VoiturePersistence(Statement conn, Connection connexion, CategoriePersistence cp, CarburantPersistence carbup, StatePersistence stp) throws ClassNotFoundException, SQLException {
         this.conn = conn;
         this.cp = cp;
         this.carbup = carbup;
-        this.connexion =  connexion;
+        this.connexion = connexion;
         this.stp = stp;
     }
 
     private Voiture createVoiture(ResultSet rs) throws SQLException {
-        return new Voiture(
-                rs.getInt("id"),
-                rs.getString("marque"),
-                rs.getString("model"),
-                rs.getInt("kilometers"),
-                rs.getInt("endommage")==1?true:false,
-                rs.getInt("vitesse")==1?true:false,
-                rs.getInt("clim")==1?true:false,
-                agence, cp.getCategorieAvecId(rs.getInt("categorie_id")),
-                carbup.getCarburantAvecId(rs.getInt("carburant_id")),
-                stp.getStateAvecId(rs.getInt("state_id"))
-                );
+        return new Voiture(rs.getInt("id"), rs.getString("marque"), rs.getString("model"), rs.getInt("kilometers"), rs.getInt("endommage") == 1, rs.getInt("vitesse") == 1, rs.getInt("clim") == 1, this.cp.getCategorieAvecId(rs.getInt("categorie_id")), this.carbup.getCarburantAvecId(rs.getInt("carburant_id")), this.stp.getStateAvecId(rs.getInt("state_id")));
     }
+
     public Voiture getVoitureAvecId(Integer id) throws SQLException {
         Statement con = super.getConn();
-        ArrayList<Voiture> listeVoitures = new ArrayList<Voiture>();
-        ResultSet rs = con.executeQuery("Select * from voiture where id="+id);
-        if(rs.next() == false)
-            return null;
-        return createVoiture(rs);
-
+        new ArrayList();
+        ResultSet rs = con.executeQuery("Select * from voiture where id=" + id);
+        return !rs.next() ? null : this.createVoiture(rs);
     }
+
     public ArrayList<Voiture> getVoitures() throws SQLException {
         Statement con = super.getConn();
-        ArrayList<Voiture> listeVoitures = new ArrayList<Voiture>();
+        ArrayList<Voiture> liste = new ArrayList();
         ResultSet rs = con.executeQuery("Select * from voiture");
-        while(rs.next()){
-            listeVoitures.add(createVoiture(rs));
+
+        while(rs.next()) {
+            liste.add(this.createVoiture(rs));
         }
-        return listeVoitures;
+
+        this.listeVoiture = liste;
+        return liste;
     }
 
-    public boolean insertVoiture(Voiture vt) throws SQLException {
-        PreparedStatement ps = connexion.prepareStatement("insert into voiture (marque,model,kilometers,endommage,vitesse,clim,categorie_id,carburant_id,state_id) values (?,?,?,?,?,?,?,?,?)");
+    public int insertVoiture(Voiture vt) throws SQLException {
+        PreparedStatement ps = this.connexion.prepareStatement("insert into voiture (marque,model,kilometers,endommage,vitesse,clim,categorie_id,carburant_id,state_id) values (?,?,?,?,?,?,?,?,?)", 1);
         ps.setString(1, vt.getMarque());
-        ps.setString(2,vt.getModel());
-        ps.setInt(3,vt.getKilometers());
-        ps.setBoolean(4,vt.isEndommage());
-        ps.setBoolean(5,vt.isVitesse());
-        ps.setBoolean(6,vt.isClim());
-        ps.setInt(7,cp.getIdCategorie(vt.getCategorie()));
-        ps.setInt(8,carbup.getIdCarbu(vt.getCarburant()));
-        ps.setInt(9,stp.getIdState(vt.getState()));
-        return ps.execute();
+        ps.setString(2, vt.getModel());
+        ps.setInt(3, vt.getKilometers());
+        ps.setBoolean(4, vt.isEndommage());
+        ps.setBoolean(5, vt.isVitesse());
+        ps.setBoolean(6, vt.isClim());
+        ps.setInt(7, this.cp.getIdCategorie(vt.getCategorie()));
+        ps.setInt(8, this.carbup.getIdCarbu(vt.getCarburant()));
+        ps.setInt(9, this.stp.getIdState(vt.getState()));
+        int retid = ps.executeUpdate();
+        vt.setId(retid);
+        this.listeVoiture.add(vt);
+        return retid;
     }
 
     public int updateVoiture(int id, Voiture vt) throws SQLException {
-        PreparedStatement ps = connexion.prepareStatement("update voiture set marque = ? ,model = ? ,kilometers = ? ,endommage = ? ,vitesse = ? ,clim = ? ,categorie_id = ? ,carburant_id = ?, state_id = ? where id = ?");
+        PreparedStatement ps = this.connexion.prepareStatement("update voiture set marque = ? ,model = ? ,kilometers = ? ,endommage = ? ,vitesse = ? ,clim = ? ,categorie_id = ? ,carburant_id = ?, state_id = ? where id = ?");
         ps.setString(1, vt.getMarque());
-        ps.setString(2,vt.getModel());
-        ps.setInt(3,vt.getKilometers());
-        ps.setBoolean(4,vt.isEndommage());
-        ps.setBoolean(5,vt.isVitesse());
-        ps.setBoolean(6,vt.isClim());
-        ps.setInt(7,cp.getIdCategorie(vt.getCategorie()));
-        ps.setInt(8,carbup.getIdCarbu(vt.getCarburant()));
-        ps.setInt(9,stp.getIdState(vt.getState()));
-        ps.setInt(10,id);
-        return ps.executeUpdate();
+        ps.setString(2, vt.getModel());
+        ps.setInt(3, vt.getKilometers());
+        ps.setBoolean(4, vt.isEndommage());
+        ps.setBoolean(5, vt.isVitesse());
+        ps.setBoolean(6, vt.isClim());
+        ps.setInt(7, this.cp.getIdCategorie(vt.getCategorie()));
+        ps.setInt(8, this.carbup.getIdCarbu(vt.getCarburant()));
+        ps.setInt(9, this.stp.getIdState(vt.getState()));
+        ps.setInt(10, id);
+        int retid = ps.executeUpdate();
+        return retid;
     }
 
     public boolean deleteVoiture(int id) throws SQLException {
-        PreparedStatement ps = connexion.prepareStatement("delete from voiture where id = ? ");
-        ps.setInt(1,id);
+        PreparedStatement ps = this.connexion.prepareStatement("delete from voiture where id = ? ");
+        ps.setInt(1, id);
         return ps.execute();
     }
 
     public CategoriePersistence getCp() {
-        return cp;
+        return this.cp;
     }
 
     public void setCp(CategoriePersistence cp) {
@@ -98,10 +96,11 @@ public class VoiturePersistence extends JdbcConnexion{
     }
 
     public CarburantPersistence getCarbup() {
-        return carbup;
+        return this.carbup;
     }
 
     public void setCarbup(CarburantPersistence carbup) {
         this.carbup = carbup;
     }
 }
+
